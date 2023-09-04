902dbb65335734919e383980c5c61d76272ca567
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-09-04 11:47:35 +0530

    Added: Accordion Completed.

--- src/components/AccordionPage/AccordionPage.tsx
+++ src/components/AccordionPage/AccordionPage.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./AccordionPage.module.less";
 import Accordion from "../commonComponents/Accordion/Accordion";
 
 type Props = {};
 
-const AccordionPage = (props: Props) => {
+const AccordionPage: React.FC = (props: Props) => {
+  const [accordion1] = useState({
+    heading: "Accordion 1",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia impedit excepturi eius atque, consequuntur a optio tempora. Iste rerum libero, minus voluptatum ipsa atque soluta, exercitationem, error accusantium nihil fugit.",
+  });
+  const [accordion2] = useState({
+    heading: "Accordion 2",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia impedit excepturi eius atque, consequuntur a optio tempora. Iste rerum libero, minus voluptatum ipsa atque soluta, exercitationem, error accusantium nihil fugit.",
+  });
+  const [accordion3] = useState({
+    heading: "Accordion 3",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia impedit excepturi eius atque, consequuntur a optio tempora. Iste rerum libero, minus voluptatum ipsa atque soluta, exercitationem, error accusantium nihil fugit.",
+  });
   return (
-    <div>
-      <Accordion />
+    <div className={style["accordions-container"]}>
+      <Accordion accordion={accordion1} />
+      <br />
+      <Accordion accordion={accordion2} />
+      <br />
+      <Accordion accordion={accordion3} />
+      <br />
     </div>
   );
 };
--- src/components/commonComponents/Accordion/Accordion.tsx
+++ src/components/commonComponents/Accordion/Accordion.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Accordion.module.less";
 import chevron from "../../../assets/images/chevron.svg";
 
-type Props = {};
+type Props = {
+  accordion: {
+    heading: String;
+    description: String;
+  };
+};
 
-const Accordion = (props: Props) => {
+const Accordion = ({ accordion }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className={style["accordion-container"]}>
-      <span>This is Accordion</span>
-      <img src={chevron} alt="" />
+    <div className={style["accordion-container"]} onClick={handleAccordion}>
+      <div className={style["accordion"]}>
+        <span>{accordion?.heading}</span>
+        <img
+          src={chevron}
+          alt=""
+          className={`${isOpen && style["opened-accordion"]}`}
+        />
+      </div>
+      {isOpen && (
+        <div className={style["accordion-content"]}>
+          <span className={style["content"]}>{accordion?.description}</span>
+        </div>
+      )}
     </div>
   );
 };
